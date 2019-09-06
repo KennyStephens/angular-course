8a29d663cbb5e47fed590c0283eeec79a519949f
Kenneth Stephens <kstephens@verisk.com>
2019-09-06 07:45:47 -0600

    Directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, setTestabilityGetter } from '@angular/core';
-import { setTimeout } from 'timers';
+// import { setTimeout } from 'timers';
 
 @Component({
   // selector: '[app-servers]',
@@ -16,6 +16,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName ="Test Server";
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout(() => {
@@ -27,6 +29,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
